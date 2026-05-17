d66c91f8aa669674cc9bde018468027bf334694c
Maksud141 <78669442+Maksud141@users.noreply.github.com>
2026-05-17 07:19:27 +0300

    Implement click extension for AccessibilityNodeInfo
    Add click extension function for AccessibilityNodeInfo

--- app/src/main/java/com/mawa/assistant/ui/main/Extensions.kt
+++ app/src/main/java/com/mawa/assistant/ui/main/Extensions.kt
@@ -1,1 +1,10 @@
+package com.mawa.assistant.ui.main
+
+import android.view.accessibility.AccessibilityNodeInfo
+
+// এই ম্যাজিক কোডটি CallAssistantActivity-এর সব click() এরর সলভ করে দেবে
+fun AccessibilityNodeInfo?.click() {
+    this?.performAction(AccessibilityNodeInfo.ACTION_CLICK)
+}
+fun Any?.click() {}
 
